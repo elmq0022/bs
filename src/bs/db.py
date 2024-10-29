f896e9eac45cfc19d13715407eeb5a6ab9fa86b8
Aaron Elmquist <aaron.elmquist@protonmail.com>
2024-10-29 08:03:00 -0500

    server runs migrations on startup

--- src/bs/db.py
+++ src/bs/db.py
@@ -5,6 +5,7 @@ from sqlalchemy.ext.asyncio import create_async_engine
 from sqlalchemy.orm import sessionmaker
 from sqlmodel.ext.asyncio.session import AsyncSession
 from pathlib import Path
+from alembic import command, config
 
 
 parent = Path(__file__).parent
@@ -12,8 +13,23 @@ sqlite_file_name = parent / "db.sqlite3"
 sqlite_file_name = str(sqlite_file_name.absolute())
 sqlite_url = f"sqlite+aiosqlite:///{sqlite_file_name}"
 
+alembic_ini = parent / "alembic.ini"
+alembic_script = parent / "migrations"
+
 connect_args = {"check_same_thread": False}
 engine = create_async_engine(sqlite_url, connect_args=connect_args)
+
+
+def run_upgrade(connection, cfg):
+    cfg.attributes["connection"] = connection
+    command.upgrade(cfg, "head")
+
+
+async def run_async_upgrade():
+    cfg = config.Config(str(alembic_ini.absolute()))
+    cfg.set_main_option("script_location", "bs:migrations")
+    async with engine.begin() as conn:
+        await conn.run_sync(run_upgrade, cfg)
 
 
 async def get_session():
--- src/bs/main.py
+++ src/bs/main.py
@@ -1,6 +1,17 @@
 from fastapi import FastAPI
 
-from bs import articles
+from contextlib import asynccontextmanager
 
-app = FastAPI()
+from bs.routers import articles
+from bs.db import run_async_upgrade
+
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    await run_async_upgrade()
+    yield
+
+
+app = FastAPI(lifespan=lifespan)
+
 app.include_router(router=articles.router)
